3fa2b67ceb31ce9bdf9ecd4c9a1dc9a3b0dc33f2
tadeo <coritocorito@hotmail.com>
2019-07-09 15:31:01 -0500

    Listo -Validar que el nombre sea de 60 posiciones en la base de datos -Validar que el rfc sea de 13 posiciones en la base de datos -Validar que el domicilio sea de 60 posiciones en la base de datos -Validar que el telefono sea de 25 posiciones en la base de datos -Validar que la ciudad sea de 20 posiciones en la base de datos -Validar que el estado sea de 20 posiciones en la base de datos -Validar que el email sea de 50 posiciones en la base de datos -Validar que el nombre sea de 60 posiciones en la vista -Validar que el rfc sea de 13 posiciones en la vista -Validar que el domicilio sea de 60 posiciones en la vista -Validar que la ciudad sea de 20 posiciones en la vista -Validar que la estado sea de 20 posiciones en la vista -Validar que el email sea de 50 posiciones en la vista -Validar que el codigo postal sea de 5 posiciones en la vista -Validar que si el codigo postal no es de 5 posiciones no permita avanzar y mande mensaje -En los campos de la alta o modificacion del cliente ponerles titulo por que se pierden cuando ya tienen datos cual es de cual -En la pantalla de crear o editar cuenta ponerle titulo por que se pierde cuando ya hay datos escritos

--- Pendvlo/Models/Customer.cs
+++ Pendvlo/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,14 +10,31 @@ namespace Pendvlo.Models
     public class Customer
     {
         public int ID { get; set; }
+
+        [MaxLength(60)]
         public string RazonSocial { get; set; }
+
+        [MaxLength(60)]
         public string Name { get; set; }
+
+        [MaxLength(13)]
         public string RFC { get; set; }
+
+        [MaxLength(60)]
         public string Address { get; set; }
+
         public int CP { get; set; }
+
+        [MaxLength(25)]
         public string Phone { get; set; }
+
+        [MaxLength(20)]
         public string City { get; set; }
+
+        [MaxLength(20)]
         public string State { get; set; }
+
+        [MaxLength(50)]
         public string email { get; set; }
         public bool Invoice { get; set; }
         public User SalesMan { get; set; }
